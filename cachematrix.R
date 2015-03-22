7eabebd179e0c4b8ce2529e0ab7bcc8679b36e2d
pradnyanigade <pradnya.nigade14@gmail.com>
2015-03-22 18:23:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+# See README.md to get a brief overview of what is to be done in the assignment
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#Function 1
+# makeCacheMatrix is a function that returns a list of functions
+# Its purpose is to store a martix and a cached value of the inverse of the matrix. 
+# Contains the following functions:
+# * setMatrix      set the value of a matrix
+# * getMatrix      get the value of a matrix
+# * cacheInverse   get the cached value (inverse of the matrix)
+# * getInverse     get the cached value (inverse of the matrix)
+#
+# Notes:
+# numeric() creates a double-precision vector of the specified length with each element equal to 0
+# "x = numeric()" creates a variable "x" that is not reachable from the global environment, 
+# but is available in the environment of the makeCacheMatrix function.
+# The <<- operator can be used to assign a value to an object in an environment 
+# that is different from the current environment. 
