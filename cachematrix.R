57a8321436285c963fc75c23bf6c6553f47ad68c
pradnyanigade <pradnya.nigade14@gmail.com>
2015-03-22 18:25:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -29,3 +29,21 @@ makeCacheMatrix <- function(x = numeric()) {
                 # since the matrix is assigned a new value, flush the cache
                 cache <<- NULL
         }
+  # returns the stored matrix
+        getMatrix <- function() {
+                x
+        }
+
+        # cache the given argument 
+        cacheInverse <- function(solve) {
+                cache <<- solve
+        }
+
+        # get the cached value
+        getInverse <- function() {
+                cache
+        }
+        
+        # return a list. Each named element of the list is a function
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+}
